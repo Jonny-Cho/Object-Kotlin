7ef74a6258b99a2dca1e82d0c7f98fc9b8539380
jonny-cho <jonny.cho@musinsa.com>
2025-04-27 15:11:50 +0900

    fix: Money > BigDecimal scale 비교하지 않도록 수정
    fix: 테스트코드 isEqualByComparingTo -> isEqualTo로 수정

--- src/test/kotlin/com/example/reserve/discount/policy/AmountDiscountPolicyTest.kt
+++ src/test/kotlin/com/example/reserve/discount/policy/AmountDiscountPolicyTest.kt
@@ -10,43 +10,43 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 import kotlin.time.Duration.Companion.minutes
 
-private val dummyMovie = Movie(
-    title = "Test Movie",
-    runningTime = 120.minutes,
-    fee = Money.wons(10000),
-    discountPolicy = object : DiscountPolicy() {
-        override fun getDiscountAmount(screening: Screening): Money = Money.ZERO
-    }
-)
-
-// dummyScreening 정의 시 업데이트된 dummyMovie 사용
-private val dummyScreening = Screening(movie = dummyMovie, sequence = 1, startTime = LocalDateTime.now())
-
 class AmountDiscountPolicyTest {
+    private val screening = Screening(
+        movie = Movie(
+            title = "Test Movie",
+            runningTime = 120.minutes,
+            fee = Money.wons(10000),
+            discountPolicy = object : DiscountPolicy() {
+                override fun getDiscountAmount(screening: Screening): Money = Money.ZERO
+            },
+        ),
+        sequence = 1,
+        startTime = LocalDateTime.now(),
+    )
 
     private val discountAmount = Money.wons(1000)
 
     @Test
     fun `할인 조건 만족 시 고정 금액 할인`() {
         val policy = AmountDiscountPolicy(discountAmount, SatisfiedCondition)
-        assertThat(policy.calculateDiscountAmount(dummyScreening)).isEqualTo(discountAmount)
+        assertThat(policy.calculateDiscountAmount(screening)).isEqualTo(discountAmount)
     }
 
     @Test
     fun `할인 조건 불만족 시 할인 없음 (ZERO)`() {
         val policy = AmountDiscountPolicy(discountAmount, NotSatisfiedCondition)
-        assertThat(policy.calculateDiscountAmount(dummyScreening)).isEqualTo(Money.ZERO)
+        assertThat(policy.calculateDiscountAmount(screening)).isEqualTo(Money.ZERO)
     }
 
     @Test
     fun `여러 조건 중 하나 만족 시 고정 금액 할인`() {
         val policy = AmountDiscountPolicy(discountAmount, NotSatisfiedCondition, SatisfiedCondition)
-        assertThat(policy.calculateDiscountAmount(dummyScreening)).isEqualTo(discountAmount)
+        assertThat(policy.calculateDiscountAmount(screening)).isEqualTo(discountAmount)
     }
 
     @Test
     fun `여러 조건 모두 불만족 시 할인 없음 (ZERO)`() {
         val policy = AmountDiscountPolicy(discountAmount, NotSatisfiedCondition, NotSatisfiedCondition)
-        assertThat(policy.calculateDiscountAmount(dummyScreening)).isEqualTo(Money.ZERO)
+        assertThat(policy.calculateDiscountAmount(screening)).isEqualTo(Money.ZERO)
     }
-} 
+}

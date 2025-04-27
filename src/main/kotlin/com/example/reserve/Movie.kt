89d6df0b947a59b6059ce4815c4ea38db5565e73
jonny-cho <jonny.cho@musinsa.com>
2025-04-27 13:06:22 +0900

    feat: Moive calculateMovieFee 정의
    - DiscountCondition, DiscountCondition 관계 추가

--- src/main/kotlin/com/example/reserve/Movie.kt
+++ src/main/kotlin/com/example/reserve/Movie.kt
@@ -1,7 +1,13 @@
 package com.example.reserve
 
-class Movie {
-    fun calculatemoviewFee(screening: Screening): Money {
-        TODO("Not yet implemented")
-    }
+import com.example.reserve.discount.policy.DiscountPolicy
+import kotlin.time.Duration
+
+class Movie(
+    private val title: String,
+    private val runningTime: Duration,
+    private val fee: Money,
+    private val discountPolicy: DiscountPolicy,
+) {
+    fun calculateMovieFee(screening: Screening) = fee.minus(discountPolicy.calculateDiscountAmount(screening))
 }
